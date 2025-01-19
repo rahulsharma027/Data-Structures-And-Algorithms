6e35b99cac7530046ef241b8e2bee1f9269147e5
Rahul Sharma <88182781+rahulsharma027@users.noreply.github.com>
2025-01-19 13:34:48 +0530

    Add files via upload

---
+++ Graphs/10. Gold Mine Problem/1219. Path With Max Gold/Leetcode.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class Leetcode {
+    static int dfs(int[][] matrix, int row, int col, int[][] memo){
+        if(row < 0 || row >= matrix.length || col < 0 || 
+          col >= matrix[0].length || matrix[row][col] == 0) return 0;
+        int result = 0;
+        int temp = matrix[row][col];
+        matrix[row][col] = 0;
+        result = Math.max(result, dfs(matrix, row + 1, col, memo));
+        result = Math.max(result, dfs(matrix, row - 1, col, memo));
+        result = Math.max(result, dfs(matrix, row , col + 1, memo));
+        result = Math.max(result, dfs(matrix, row , col - 1, memo));
+        matrix[row][col] = temp;
+        return matrix[row][col] + result;
+    }
+    static int getMaximumGold(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] memo = new int[n][m];
+        int ans = 0;
+        for(int[] it : memo){
+            Arrays.fill(it, -1);
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                ans = Math.max(ans, dfs(grid, i, j, memo));
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[][] goldMine = {{1,0,7,0,0 ,0},
+                            {2,0,6,0,1 ,0},
+                            {3,5,6,7,4 ,2},
+                            {4,3,1,0,2 ,0},
+                            {3,0,5,0,20,0}};
+        System.out.println(getMaximumGold(goldMine));
+    }
+}
