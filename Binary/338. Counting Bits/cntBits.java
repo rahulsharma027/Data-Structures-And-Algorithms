c3c799f9682ae20cdaf96ee7b62d12127dac122d
Rahul Sharma <88182781+rahulsharma027@users.noreply.github.com>
2025-01-20 14:08:13 +0530

    Add files via upload

---
+++ Binary/338. Counting Bits/cntBits.java
@@ -0,0 +1,21 @@
+class cntBits{
+    static int[] countBits(int n){
+        int[] arr = new int[n+1];
+        for(int i=1; i<=n; i++){
+            if(i % 2 == 1){
+                arr[i] = arr[i >> 1] + 1;
+            }
+            else{
+                arr[i] = arr[i>>1];
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        int[] arr = countBits(n);
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
